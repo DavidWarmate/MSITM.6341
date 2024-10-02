394bc44271a92b96c293c84baebca05ce21c2e15
jwjeon712 <jeonjw712@gmail.com>
2024-10-01 22:50:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 # This is Jiwon's app.py file
+
+# Exercise 1: Calculate the Sum of Two Numbers 
+# 1. Start
+
+# 2. Input number1
+number1 = float(input("Enter number1: "))
+
+# 3. Input number2
+number2 = float(input("Enter number2: "))
+
+# 4. sum = number1 + number2
+sum = number1 + number2
+
+# 5. Print sum
+print("The sum of number1 and number2 is: ", sum)
+
+# 6. End
